8a535f9f005880dc82c5d26418af44b935bd3470
deji12 <adesolaayodeji53@gmail.com>
2022-09-18 17:20:27 +0800

    deploying to railway

--- tdl/settings.py
+++ tdl/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
-#   
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,7 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-# django_heroku.settings(locals())
 
 LOGIN_URL = 'login'
 
