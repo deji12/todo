169bc2912ce184bdcdee5765ea1efe1bc0cfab27
Ayodeji Adesola <82943610+deji12@users.noreply.github.com>
2023-01-21 07:13:13 +0300

    deployment for railway

--- tdl/settings.py
+++ tdl/settings.py
@@ -150,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
+# https://docs.djangoproject.com/en/4.0/topics/i18n/s
 
 LANGUAGE_CODE = 'en-us'
 
